a3b2b4e054519ae9fdac591a050881325c4a79ba
Pieti <niko.pietikainen@gmail.com>
2015-12-14 15:11:34 +0200

    stt + tts

--- sop.py
+++ sop.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import speech_recognition as sr
+from gtts import gTTS
+import subprocess
+
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -10,8 +13,15 @@ with sr.Microphone() as source:
 
 #recognize speech using Google Speech Recognition 
 try:
-	print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+	audio_string = r.recognize_google(audio)
+	print("Google Speech Recognition thinks you said " + audio_string)
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+text = "I think you said " + audio_string
+subprocess.call('echo '+text+'|festival --tts', shell=True)
+
+
+
