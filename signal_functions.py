be337904d219273e857bf215bdac40d11dfdbc0e
nikolehto <niko.lehto@mbnet.fi>
2016-02-02 14:38:27 +0200

    update
    plotAll funktio tulostaa audion ja spektrogrammin annetulla aikavälillä.
    Lisäksi tarkasteltavia taajuuksia voi rajoittaa.
    
    Kaikki muuttujat määritellään alussa

--- signal_functions.py
+++ signal_functions.py
@@ -1,22 +1,58 @@
 from scipy import signal
-import scipy.io.wavfile as wav
+from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def plotAll(f,t,Sxx, fs, audio, ylimits = 0, xlimits = 0):
+    plt.subplot(2,1,1)
+    audio_sampletimes = range(len(audio))
+    x_axis = [i / fs for i in audio_sampletimes]
+
+    if xlimits:
+        plt.xlim(xlimits)
+    plt.plot(x_axis, audio)
+    plt.xlabel('Time (micro seconds)')
+    plt.ylabel('Amplitude')
+
+    plt.subplot(2,1,2)
+    plt.pcolormesh(t, f, Sxx)
+    if ylimits:
+        plt.ylim(ylimits)
+    if xlimits:
+        plt.xlim(xlimits)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+
+def plotSpectrogram(f,t,Sxx):
+    plt.pcolormesh(t, f, Sxx)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+# Spectrogram variables
+overlapBetweenSegments = 0
+lengthOfFFT = 32
+lengthOfSegment = 0 # Must be greater or equal to lengthOfFFT
+
+# Time Limit in seconds
+begin = 0 # start time
+end = 6.7 # end time
+
+timelimit = [begin, end]
+
+if lengthOfSegment < lengthOfFFT:
+    lengthOfSegment = lengthOfFFT
+
+# Korvaa tämä omalla ääninäytteellä
 audio_sample_location = r'C:\Koulu\aaninayte\OSR_us_000_0030_8k.wav'
 
-fs, audio = wav.read(audio_sample_location)
-"""
-N = 1e5
-amp = 2 * np.sqrt(2)
-noise_power = 0.001 * fs / 2
-time = np.arange(N) / fs
-freq = np.linspace(1e3, 2e3, N)
-x = amp * np.sin(2*np.pi*freq*time)
-x += np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-"""
-f, t, Sxx = signal.spectrogram(audio, fs)
-plt.pcolormesh(t, f, Sxx)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.show()
+fs, audio = wavfile.read(audio_sample_location)
+
+f, t, Sxx = signal.spectrogram(audio, fs, nfft=lengthOfFFT, nperseg=lengthOfSegment, noverlap=overlapBetweenSegments)
+print(fs)
+#plotSpectrogram(f, t, Sxx)
+plotAll(f, t, Sxx,fs, audio, xlimits=timelimit)
+
